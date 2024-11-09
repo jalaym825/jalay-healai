80af4de1aa896a61cc7108aae7dc0d7b74fc7d3f
Jalay Movaliya <jalaym825@gmail.com>
2024-11-10 03:22:16 +0530

    get token from body

--- backend/src/api/appointment/getToken.js
+++ backend/src/api/appointment/getToken.js
@@ -3,15 +3,15 @@ const jwt = require("jsonwebtoken")
 
 const getToken = asyncHandler(async (req, res, next) => {
 
-    const userID = req.user.email.split('@')[0];
+    const {userId} = req.body;
 
-    if (!userID) {
+    if (!userId) {
         return next({
             path: '/appointment/getToken', statusCode: 400, message: "UserID is required!"
         })
     }
 
-    const token = await jwt.sign({ user_id: userID }, "wk8qegtn7utdfn3y6pzpnc7sw4hzktqar727xfcqrtej3fhwk8xqsykweun54s5a");
+    const token = await jwt.sign({ user_id: userId }, "wk8qegtn7utdfn3y6pzpnc7sw4hzktqar727xfcqrtej3fhwk8xqsykweun54s5a");
 
     return res.status(200).json({
         message: "Token fetched successfully!",
