82cc6b1fc934473e9144fa49071062093dbb0bd5
Jalay Movaliya <jalaym825@gmail.com>
2024-11-10 03:09:02 +0530

    fix: create appt

--- backend/src/api/appointment/createAppointment.js
+++ backend/src/api/appointment/createAppointment.js
@@ -1,9 +1,5 @@
 const asyncHandler = require("express-async-handler");
-const bcrypt = require("bcrypt");
-const crypto = require("crypto");
-const jwt = require("jsonwebtoken");
 const { Prisma } = require("../../utils/index");
-
 
 function generateSixCharString() {
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -15,7 +11,6 @@ function generateSixCharString() {
 
     return result;
 }
-
 
 const createAppointment = asyncHandler(async (req, res, next) => {
     const {
@@ -30,57 +25,49 @@ const createAppointment = asyncHandler(async (req, res, next) => {
 
     const date = new Date(time);
     const isValid = date < Date.now();
-
     const meeting_id = generateSixCharString();
 
     if (!isValid) {
         return next({
             path: '/appointment/createAppointment', statusCode: 400, message: "Invalid date and time!"
-        })
+        });
     }
 
-
-    if (hosted_by == attended_by) {
+    if (hosted_by === attended_by) {
         return next({
-            path: '/appointment/createAppointment', statusCode: 400, message: "Hosted user and Attendee user cannot be same!"
-        })
+            path: '/appointment/createAppointment', statusCode: 400, message: "Hosted user and Attendee user cannot be the same!"
+        });
     }
 
     const hosted_user = await Prisma.users.findFirst({
-        where: {
-            email: hosted_by
-        }
-    })
+        where: { email: hosted_by }
+    });
 
     if (!hosted_user) {
-        return next({ path: '/appointment/createAppointment', statusCode: 404, message: "Hosted user is not found!" })
+        return next({ path: '/appointment/createAppointment', statusCode: 404, message: "Hosted user not found!" });
     }
 
     const attendee_user = await Prisma.users.findFirst({
-        where: {
-            email: attended_by
-        }
-    })
+        where: { email: attended_by }
+    });
 
     if (!attendee_user) {
-        return next({ path: '/appointment/createAppointment', statusCode: 404, message: "Attendee user is not found!" })
+        return next({ path: '/appointment/createAppointment', statusCode: 404, message: "Attendee user not found!" });
     }
-
 
     await Prisma.appointment.create({
         data: {
-            hosted_by,
-            attended_by,
             time,
-            appointment_link: meeting_id
+            appointment_link: meeting_id,
+            hosted_by_user: { connect: { id: hosted_user.id } },
+            attended_by_user: { connect: { id: attendee_user.id } }
         }
-    })
+    });
 
     return res.status(201).json({
-        message: "Appointment Schedule Successfully!",
+        message: "Appointment Scheduled Successfully!",
         meeting_id
-    })
-
-})
+    });
+});
 
 module.exports = createAppointment;
