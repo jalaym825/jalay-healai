861f3ab8a69c78c9c221de602dac41c8bf54e566
Jalay Movaliya <jalaym825@gmail.com>
2024-11-10 00:35:23 +0530

    add /getAvailableDoctors api

--- backend/src/api/appointment/router.js
+++ backend/src/api/appointment/router.js
@@ -4,12 +4,14 @@ const { AppointmentSchema } = require('./zodSchemas')
 const createAppointment = require('./createAppointment');
 const getAppointment = require("./getAppointment")
 const getToken = require('./getToken')
-const { verifyJWT } = require('../../middlewares/index')
+const { verifyJWT } = require('../../middlewares/index');
+const getAvailableDoctors = require('./getAvailableDoctors');
 
 const router = Router();
 
 router.post("/createAppointment", verifyJWT, validateZodSchema(AppointmentSchema), createAppointment)
 router.get('/getAppointment', verifyJWT, getAppointment)
 router.post('/getToken', verifyJWT, getToken)
+router.get('/getAvailableDoctors', verifyJWT, getAvailableDoctors)
 
 module.exports = router;
