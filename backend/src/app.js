c349d4b816ec221046251da82aabe1db8c0db810
Jalay Movaliya <jalaym825@gmail.com>
2024-11-09 11:18:22 +0530

    express server setup with cors, morgan logger, helmet

---
+++ backend/src/app.js
@@ -0,0 +1,36 @@
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const express = require('express');
+const helmet = require('helmet');
+const http = require('http');
+const morgan = require('morgan');
+
+const app = express();
+const server = http.createServer(app);
+
+app.use(morgan("[:date[clf]] :method :url :status :res[content-length] - :response-time ms"));
+
+app.use(cors({
+  origin: ['http://localhost:5173'],
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true
+}));
+
+app.use(express.json());
+app.use(helmet());
+app.use(cookieParser());
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+  console.log(`Worker ${process.pid} is listening on port ${port}`);
+});
+
+app.get("/", (req, res) => {
+  res.json("Hello World!!!");
+})
+
+process.on("unhandledRejection", (reason, p) => {
+  logger.debug(
+    `Unhandled Rejection at:  Promise ${p} reason: ${reason}`
+  );
+});
